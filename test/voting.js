ebe5a006813c6e9ca2de98472a523467049b0b67
sovni <christophe.burgalat@gmail.com>
2022-01-09 15:37:04 +0100

    Improve javascript tests

--- test/voting.js
+++ test/voting.js
@@ -23,6 +23,10 @@ contract("Voting", accounts => {
         await truffleAssert.fails(votingInstance.voterRegistration(accounts[2], { from:address }));
     });
 
+    it("...making new proposal should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.proposalRegistration("Proposal 4", { from:accounts[4] }));
+    });
+
     it("...voter registration for 4 addresses", async () => {
         await votingInstance.voterRegistration(accounts[2], { from:ownerAddress });
         await votingInstance.voterRegistration(accounts[3], { from:ownerAddress });
@@ -42,6 +46,10 @@ contract("Voting", accounts => {
         assert.equal(status, Voting.WorkflowStatus.ProposalsRegistrationStarted, "Voting status should be ProposalsRegistrationStarted");
     });
 
+    it("...registering new voters should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.voterRegistration(accounts[6], { from:ownerAddress }));
+    });
+
     it("...make 3 different proposals from 3 different addresses", async () => {
         await votingInstance.proposalRegistration("Proposal 1", { from:ownerAddress });
         await votingInstance.proposalRegistration("Proposal 2", { from:address });
@@ -50,14 +58,34 @@ contract("Voting", accounts => {
         assert.equal(3, nbProposal, "There should be 3 proposals");
     });
 
+    it("...voting should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.voting(1, { from:accounts[2] }));
+    });
+
     it("...stop proposal registration phase should not be allowed if no owner", async () => {
         await truffleAssert.fails(votingInstance.stopProposalsRegistration({ from:address }));
+    });
+
+    it("...voting should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.voting(1, { from:accounts[2] }));
     });
 
     it("...stop proposal registration phase", async () => {
         await votingInstance.stopProposalsRegistration({from: ownerAddress });
         const status = await votingInstance.getVotingStatus({from: address });
         assert.equal(status, Voting.WorkflowStatus.ProposalsRegistrationEnded, "Voting status should be ProposalsRegistrationEnded");
+    });
+
+    it("...making new proposal should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.proposalRegistration("Proposal 4", { from:accounts[4] }));
+    });
+
+    it("...registering new voters should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.voterRegistration(accounts[6], { from:ownerAddress }));
+    });
+
+    it("...voting should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.voting(1, { from:accounts[2] }));
     });
 
     it("...start voting phase should not be allowed if no owner", async () => {
@@ -68,5 +96,48 @@ contract("Voting", accounts => {
         await votingInstance.startVotingSession({from: ownerAddress });
         const status = await votingInstance.getVotingStatus({from: address });
         assert.equal(status, Voting.WorkflowStatus.VotingSessionStarted, "Voting status should be VotingSessionStarted");
+    });
+
+    it("...registering new voters should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.voterRegistration(accounts[6], { from:ownerAddress }));
+    });
+
+    it("...making new proposal should not be allowed during this phase", async () => {
+        await truffleAssert.fails(votingInstance.proposalRegistration("Proposal 4", { from:accounts[4] }));
+    });
+
+    it("...vote from unregistered address is not allowed", async () => {
+        await truffleAssert.fails(votingInstance.voting(1, { from:address }));
+    });
+
+    it("...vote from unexisting proposal  is not allowed", async () => {
+        await truffleAssert.fails(votingInstance.voting(50, { from:accounts[2] }));
+    });
+
+    it("...apply vote for the registered addresses", async () => {
+        await votingInstance.voting(1, { from:accounts[2] });
+        await votingInstance.voting(2, { from:accounts[3] });
+        await votingInstance.voting(3, { from:accounts[4] });
+        await votingInstance.voting(2, { from:accounts[5] });
+    });
+
+    it("...stop voting phase should not be allowed if no owner", async () => {
+        await truffleAssert.fails(votingInstance.stopVotingSession({ from:address }));
+    });
+
+    it("...stop voting phase", async () => {
+        await votingInstance.stopVotingSession({from: ownerAddress });
+        const status = await votingInstance.getVotingStatus({from: address });
+        assert.equal(status, Voting.WorkflowStatus.VotesTallied, "Voting status should be VotesTallied");
+    });
+
+    it("...winning proposal should be number Proposal 2", async () => {
+        const winningProposal = await votingInstance.getWinningProposal({from: address });
+        assert.equal("Proposal 2", winningProposal, "Voting winning proposal should be Proposal 2");
+    });
+
+    it("...winning nb votes should be 2", async () => {
+        const nbVotes = await votingInstance.getWinningVotes({from: address });
+        assert.equal(2, nbVotes, "Voting winning numbers should be  2");
     });
 });
